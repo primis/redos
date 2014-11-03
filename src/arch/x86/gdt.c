8ca9bb985eb183ba429a4ec68c820bc7c5fe3b3b
primis <primis@primiscomputers.com>
2014-11-03 16:19:04 -0500

    Standardization, gdt code

--- src/arch/x86/gdt.c
+++ src/arch/x86/gdt.c
@@ -1,32 +1,33 @@
-#include <kernel/gdt.h>
+#include <kernel/arch/x86/gdt.h>
 
-extern void gdt_flush(unsigned int);
+extern void gdt_flush(uint32_t);
 
-void gdtSetGate(int, unsigned long, unsigned long, unsigned char, unsigned char);
+void gdtSetGate(int, uint32_t, uint32_t, uint8_t, uint8_t);
 
 gdt_entry_t gdt[5];
 gdt_ptr_t   gdtPointer;
 
-void gdtInit() {
+void gdtInit()
+{
     gdtPointer.limit = (sizeof(gdt_entry_t)*5) - 1;
-    gdtPointer.base  = (unsigned int)&gdt;
+    gdtPointer.base  = (uint32_t)&gdt;
     
     gdtSetGate(0, 0, 0, 0, 0);
     gdtSetGate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF);
     gdtSetGate(2, 0, 0xFFFFFFFF, 0x92, 0xCF);
     gdtSetGate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF);
     gdtSetGate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF);
-    gdt_flush((unsigned int)&gdtPointer);
+    gdt_flush((uint32_t)&gdtPointer);
 }
 
-void gdtSetGate(int n, unsigned long base, unsigned long limit, 
- unsigned char access, unsigned char gran) {
+void gdtSetGate(int n, uint32_t base, uint32_t lim, uint8_t acc, uint8_t gran) 
+{
     gdt[n].base_low     = (base & 0xFFFF);
     gdt[n].base_mid     = (base >> 16) & 0xFF;
     gdt[n].base_high    = (base >> 24) & 0xFF;
-    gdt[n].limit_low    = (limit & 0xFFFF);
-    gdt[n].granularity  = ((limit >> 16) & 0x0F);
+    gdt[n].limit_low    = (lim & 0xFFFF);
+    gdt[n].granularity  = ((lim >> 16) & 0x0F);
     gdt[n].granularity  |= gran & 0xF0;
-    gdt[n].access       = access;
+    gdt[n].access       = acc;
 }
 
